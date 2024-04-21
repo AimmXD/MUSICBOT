5bd4899fb45b9f61354d39fa06466d04c5593b54
AimmXD <storejyan@gmail.com>
2024-04-21 23:40:23 +0530

    Initial Commit

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/codebitey/NezukoProBot",
+    "https://github.com/AimmXD/MUSICBOTT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
